74bad1902f5f350aa92dfb314f7c56e94ab98267
Shamsheer <Shamsheerkhalid99@gmail.com>
2023-09-16 15:46:43 +0530

    Update st_att.py

--- st_att.py
+++ st_att.py
@@ -4,9 +4,27 @@ import streamlit as st
 # Define the calculate_attendance function
 
 
+def calculate_skippable_hours(attended, total, min_criteria):
+    skipped = 0
+    while attended / total >= min_criteria:
+        total += 1
+        skipped += 1
+    return skipped - 1
+
+
+def calculate_additional_hours(attended, total, min_criteria):
+    additional_hours = 0
+    while attended / total < min_criteria:
+        attended += 1
+        total += 1
+        additional_hours += 1
+    return additional_hours
+
+
 def calculate_attendance(subjects, text):
     # Initialize dictionaries to store counts for each subject
-    subject_counts = {subject: {"P": 0, "U": 0} for subject in subjects}
+    subject_counts = {subject: {"P": 0, "U": 0, "Skippable Hours": 0}
+                      for subject in subjects}
 
     # Iterate through subjects and count attendance for each
     for subject in subjects:
@@ -23,6 +41,14 @@ def calculate_attendance(subjects, text):
                 subject_counts[subject]["P"] += 1
             elif status == "U":
                 subject_counts[subject]["U"] += 1
+
+        # Calculate skippable hours for the current subject
+        attended_hours = subject_counts[subject]["P"]
+        min_attendance_criteria = 0.8
+        total_hours = attended_hours + subject_counts[subject]["U"]
+        skippable_hours = calculate_skippable_hours(
+            attended_hours, total_hours, min_attendance_criteria)
+        subject_counts[subject]["Skippable Hours"] = skippable_hours
 
     # Create a dictionary to store results
     results = {}
@@ -41,6 +67,7 @@ def calculate_attendance(subjects, text):
             "P": count_p,
             "U": count_u,
             "Percentage of Hours Attended": attendance_percentage,
+            "Skippable Hours": subject_counts[subject]["Skippable Hours"]
         }
 
     return results
@@ -76,6 +103,12 @@ def main():
                 f"Total number of hours attended: {results['P']}/{results['P'] + results['U']}")
             st.write(
                 f"Percentage: {results['Percentage of Hours Attended']:.2f}%")
+            if results['Skippable Hours'] >= 0:
+                st.write(f"Skippable Hours: {results['Skippable Hours']}")
+            else:
+                additional = calculate_additional_hours(
+                    results['P'], results['P']+results['U'], 0.8)
+                st.write(f"You need to attend {additional} more hours.")
             st.write("---")
 
 
